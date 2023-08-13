f4772049c81a4598ce5ccd1896911b2d31273b35
Mayuri <bhamaremayuri12@gmail.com>
2023-08-13 12:41:49 +0530

    assignment 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,26 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args)
+    {
+        Product p = new Product();
+        p.product(1,5);
+        p.product(1,5,2);
+        p.product(5.02,3.01);
+    }
+    public static class Product
+{
+    public int product(int x,int y)
+    {
+        return x*y;
+    }
+    public int product(int x,int y,int z)
+    {
+        return x*y*z;
+    }
+    public double product(double x,double y)
+    {
+        return x*y;
+    }
+}
 }
